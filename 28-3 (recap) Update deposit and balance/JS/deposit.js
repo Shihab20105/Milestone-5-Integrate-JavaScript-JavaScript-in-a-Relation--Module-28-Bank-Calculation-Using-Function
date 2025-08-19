a28c28ffc13c776b5b9cd8dae317dba196865067
shihab20105 <shihabur10@gmail.com>
2025-08-19 23:27:51 +0100

    Discovered (recap) Update deposit and balance and read code

---
+++ 28-3 (recap) Update deposit and balance/JS/deposit.js
@@ -0,0 +1,52 @@
+/*                          28-3 (recap) Update deposit and balance and read code */
+//console.log('Deposit File');
+
+/* 
+Step-1: Add event listener to the deposit button
+
+Step-2: Get deposit amount from the deposit input field
+    2-1: Convert string deposit amount
+
+Step-3: Clear the deposit input field after getting the value
+
+Step-4: Get the previous deposit total
+
+Step-5: Calculate new deposit total and set the value to the deposit total
+
+Step-6: Get current balance total
+
+Step-7: Calculate the new balance and set it to the balance total element
+*/
+
+//Step-1
+document.getElementById('btn-deposit').addEventListener('click', function(){
+    //console.log('Deposit button clicked');
+
+    //Step-2
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    //console.log(newDepositAmountString);
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    //console.log(newDepositAmount);
+
+    //Step-3
+    depositField.value = '';
+
+    //Step-4
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    //Step-5
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
+
+    //Step-6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+    //Step-7
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+})
---
+++ 28-3 (recap) Update deposit and balance/JS/withdraw.js
@@ -0,0 +1,12 @@
+/*                          28-3 (recap) Update deposit and balance and read code */
+
+//console.log('Withdraw JS Connected');
+
+/* 
+Step-1: Add event handler with the withdraw button
+*/
+
+//Step-1
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    console.log('Withdraw Button Clicked');
+})
